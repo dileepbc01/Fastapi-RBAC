f569d8a5a09686d322e6588027c4aba3ce576f0d
Dileep B C <dileepbc01@gmail.com>
2025-04-30 22:14:00 +0530

    get curr user details

--- app/api/routes/auth.py
+++ app/api/routes/auth.py
@@ -8,12 +8,12 @@ from app.models import user
 from fastapi import HTTPException,status
 from sqlmodel import select
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from app.models import token
-from app.utils import jwt,hash
+from app.models.token import Token
+from app.utils import create_acc_token, hash
 from datetime import datetime,timedelta
 from app.config import settings
 from jwt.exceptions import InvalidTokenError
-
+import jwt
 
 router = APIRouter(
     prefix="/auth",
@@ -25,19 +25,37 @@ router = APIRouter(
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
 
 
+def get_current_user(db: SessionDep , token: str = Depends(oauth2_scheme), ) -> User:
+    credentials_exception = HTTPException(
+    status_code=status.HTTP_401_UNAUTHORIZED,
+    detail="Could not validate credentials",
+    headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token,settings.secret_key,algorithms=[settings.algorithm])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+    except InvalidTokenError:
+        raise credentials_exception
+    
+    user = db.exec(select(User).where(User.username == username)).first()
+    if user is None:
+        raise credentials_exception
+    return user
+
+
+
 
 @router.post("/register")
 async def register(user_data: user.UserCreate, db: SessionDep) -> None:
-    # Check if the user already exists
     existing_user = db.exec(select(User).where(User.username == user_data.username)).first()
     
     if existing_user:
         raise HTTPException(status_code=400, detail="Username already taken")
     
-    # Hash password before saving (using a library like passlib)
     hashed_password = hash.get_password_hash(user_data.password)  # Replace with actual hashing logic
     
-    # Create new user instance
     new_user = User(username=user_data.username,role=user_data.role,password_hash=hashed_password)
 
     
@@ -52,7 +70,7 @@ async def register(user_data: user.UserCreate, db: SessionDep) -> None:
 async def login_user(
        form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
        db:SessionDep
-) -> token.Token:
+) -> Token:
     user_data= db.exec(select(User).where(User.username == form_data.username)).first()
     if not user_data:
         raise HTTPException(
@@ -61,7 +79,13 @@ async def login_user(
             headers={"WWW-Authenticate": "Bearer"},
         )
     access_token_expires = timedelta(minutes=settings.access_token_expire_minutes)
-    access_token = jwt.create_access_token(
+    access_token = create_acc_token.create_access_token(
         data={"sub": user_data.username}, expires_delta=access_token_expires
     )
-    return token.Token(access_token=access_token, token_type="bearer")
+    return Token(access_token=access_token, token_type="bearer")
+
+
+
+@router.get("/me")
+def getMe(currentUser:User = Depends(get_current_user)):
+    return currentUser
