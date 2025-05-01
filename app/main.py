b50812f88d4b3defe4ca13aa3df17a29d3d64d6c
Dileep B C <dileepbc01@gmail.com>
2025-05-01 15:45:49 +0530

    fix : main

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ import uvicorn
 from contextlib import asynccontextmanager
 from app.api import routes 
 from app.config import settings
-
+from app.utils.enums import AppEnvironmentEnum
 logging.basicConfig(stream=sys.stdout, level=settings.log_level.value)
 
 @asynccontextmanager
@@ -23,4 +23,4 @@ async def root():
     return {"message": "Hello World"}
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=settings.env==AppEnvironmentEnum.DEVELOPMENT.value, log_level=settings.log_level.value)
